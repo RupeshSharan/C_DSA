7995f98961c50171d4810993e631e52b0802e282
RupeshSharan <147916199+RupeshSharan@users.noreply.github.com>
2024-08-31 13:12:06 +0530

    Create alternatestrings.c

---
+++ alternatestrings.c
@@ -0,0 +1,1 @@
+//ex:- A="abcd" and B="pqr" concatinate such that they come alternately  like s="a p b q c r d"  assumption:- storing first word in odd indices"(i+1)" and second word in even indices 
